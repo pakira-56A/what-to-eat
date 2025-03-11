b4f185c73d9571c0edc4a7e287b8f219e4ab37fc
pakira-56A <pakira.rrr@gmail.com>
2025-03-11 21:24:23 +0900

    fix: `js`ファイルに作り替え

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuizApp from './quiz-app';
+
+function App() {
+  return (
+    <div className="App">
+      <QuizApp />
+    </div>
+  );
+}
+
+export default App;
