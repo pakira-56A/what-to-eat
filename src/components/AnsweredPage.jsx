b95e0c3d3a1d0000d886c77982253d84e63f48d5
pakira-56A <pakira.rrr@gmail.com>
2025-03-13 16:30:25 +0900

    fix: 共通化したボタンのスタイルを、各コンポーネントに呼び出し

--- src/components/AnsweredPage.jsx
+++ src/components/AnsweredPage.jsx
@@ -1,13 +1,4 @@
-const commonButtonStyle = {
-  padding: "10px 25px",
-  border: "none",
-  borderRadius: "10px",
-  color: "white",
-  fontSize: "20px",
-  cursor: "pointer",
-  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-  transition: "background-color 0.3s, box-shadow 0.3s",
-}
+import { globalButtonStyle } from '../styles/globalButton';
 
 export const AnsweredPage = ({nextTimeCount, quizData, justQuestion, isTimeUp, isSuccess, shuffledAnswers, selectedAnswer}) => {
 
@@ -65,7 +56,7 @@ export const AnsweredPage = ({nextTimeCount, quizData, justQuestion, isTimeUp, i
           <button
             key={index}
             style={{
-              ...commonButtonStyle,
+              ...globalButtonStyle,
               backgroundColor:
                 selectedAnswer === answer
                   ? isSuccess && answer === quizData[justQuestion].justAnswer
