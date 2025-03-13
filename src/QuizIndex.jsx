a171df128f6d2a4f7e09ddcf93960c58a7658da4
pakira-56A <pakira.rrr@gmail.com>
2025-03-13 16:29:24 +0900

    fix: 共通化していたボタンのスタイルを`src/QuizIndex.jsx`から削除

--- src/QuizIndex.jsx
+++ src/QuizIndex.jsx
@@ -163,17 +163,6 @@ const QuizApp = () => {
     setIsTimeUp(false)
   }
 
-  // 統一されたボタンスタイル
-  const commonButtonStyle = {
-    padding: "10px 25px",
-    border: "none",
-    borderRadius: "10px",
-    color: "white",
-    fontSize: "20px",
-    cursor: "pointer",
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-    transition: "background-color 0.3s, box-shadow 0.3s",
-  }
 
   return (
     <div
