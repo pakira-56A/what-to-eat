e574dc1a9b81f0c8225beff27fbb3fa9106b4fc9
pakira-56A <pakira.rrr@gmail.com>
2025-03-14 18:55:30 +0900

    fix: ボタンが複数あるコンポーネントに、ホバー時のボタンのスタイルと`useState`をインポート

--- src/components/QuizSelectionPage.jsx
+++ src/components/QuizSelectionPage.jsx
@@ -1,52 +1,47 @@
-import { globalButtonStyle } from '../styles/globalButton';
+import { useState } from 'react';
+import { globalButtonStyle, globalButtonHoverStyle } from '../styles/globalButton';
 
 export const QuizSelectionPage = ({selectedAnswer, timeCount, quizData, justQuestion, shuffledAnswers}) => {
 
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <>
-      <div>
-        <div
-          style={{
-            fontSize: "30px",
-            fontWeight: "bold",
-            color: timeCount <= 2 ? "red" : "orange",
-          }}
-        >
-          {timeCount} 秒以内に
-        </div>
-        <h3 style={{ color: "orange" }}>{`ご挨拶しよう！`}</h3>
-
-        <div style={{ marginBottom: "20px" }}>
-          <img
-            src={quizData[justQuestion].image}
-            alt="人物"
-            style={{ width: "auto", height: "250px" }}
-          />
-        </div>
-        <div style={{ display: "flex", flexDirection: "column", gap: "15px", width: "230px", margin: "0 auto" }}>
-          {shuffledAnswers.map((answer, index) => (
-            <button
-              key={index}
-              onClick={() => selectedAnswer(answer)}
-              style={{
-                ...globalButtonStyle,
-                backgroundColor: "#00BBFF", // ほどよい青
-              }}
-              onMouseOver={(e) => {
-                e.currentTarget.style.backgroundColor = "orange"
-                e.currentTarget.style.boxShadow = "none"
-              }}
-              onMouseOut={(e) => {
-                e.currentTarget.style.backgroundColor = "#00BBFF"  // ほどよい青
-                e.currentTarget.style.boxShadow = "0 4px 8px rgb(250, 150, 0, 0.6)"
-              }}
-            >
-              {answer}
-            </button>
-          ))}
-        </div>
+    <div>
+      <div
+        style={{
+          fontSize: "30px",
+          fontWeight: "bold",
+          color: timeCount <= 2 ? "red" : "orange",
+        }}
+      >
+        {timeCount} 秒以内に
       </div>
-    </>
-  )
+      <h3 style={{ color: "orange" }}>{`ご挨拶しよう！`}</h3>
 
+      <div style={{ marginBottom: "20px" }}>
+        <img
+          src={quizData[justQuestion].image}
+          alt="人物"
+          style={{ width: "auto", height: "250px" }}
+        />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "15px", width: "230px", margin: "0 auto" }}>
+        {shuffledAnswers.map((answer, index) => (
+          <button
+            key={index}
+            onClick={() => selectedAnswer(answer)}
+            style={{
+              ...globalButtonStyle,
+              backgroundColor: isHovered === index ? globalButtonHoverStyle.backgroundColor : "#00BBFF",
+              boxShadow: isHovered === index ? globalButtonHoverStyle.boxShadow : globalButtonStyle.boxShadow,
+            }}
+            onMouseOver={() => setIsHovered(index)}
+            onMouseOut={() => setIsHovered(null)}
+          >
+            {answer}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
 }
