33d8a10e582b2057aebb87c1c8dbdce323c962d1
pakira-56A <pakira.rrr@gmail.com>
2025-03-14 21:09:01 +0900

    fix: フォーマットを実行

--- src/components/AnsweredPage.jsx
+++ src/components/AnsweredPage.jsx
@@ -1,7 +1,14 @@
-import { globalButtonStyle } from '../styles/globalButton';
+import { globalButtonStyle } from "../styles/globalButton"
 
-export const AnsweredPage = ({nextTimeCount, quizData, justQuestion, isTimeUp, isSuccess, shuffledAnswers, selectedAnswer}) => {
-
+export const AnsweredPage = ({
+  nextTimeCount,
+  quizData,
+  justQuestion,
+  isTimeUp,
+  isSuccess,
+  shuffledAnswers,
+  selectedAnswer
+}) => {
   return (
     <div
       style={{
@@ -9,14 +16,14 @@ export const AnsweredPage = ({nextTimeCount, quizData, justQuestion, isTimeUp, i
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        fontFamily: "Arial, sans-serif",
+        fontFamily: "Arial, sans-serif"
       }}
-      >
+    >
       <div
         style={{
           fontSize: "30px",
           fontWeight: "bold",
-          color: "orange",
+          color: "orange"
         }}
       >
         {nextTimeCount} 秒後に
@@ -30,28 +37,36 @@ export const AnsweredPage = ({nextTimeCount, quizData, justQuestion, isTimeUp, i
           style={{ width: "auto", height: "250px" }}
         />
         <div
-          style={{   // タイムアップか正解不正解の表示
+          style={{
+            // タイムアップか正解不正解の表示
             position: "absolute",
             width: "120px",
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
             backgroundColor: isTimeUp
-              ? "rgba(130, 130, 130, 0.9)"   // グレー
+              ? "rgba(130, 130, 130, 0.9)" // グレー
               : isSuccess
-                ? "rgba(0, 255, 0, 0.9)"     // 緑
-                : "rgba(255, 0, 0, 0.9)",    // 赤
-            color:        "white",
-            padding:      "20px",
+                ? "rgba(0, 255, 0, 0.9)" // 緑
+                : "rgba(255, 0, 0, 0.9)", // 赤
+            color: "white",
+            padding: "20px",
             borderRadius: "5px",
-            fontSize:     "30px",
-            fontWeight:   "bold",
+            fontSize: "30px",
+            fontWeight: "bold"
           }}
         >
           {isTimeUp ? "時間切れ" : isSuccess ? "しゅきぃ" : "ちゃうで"}
         </div>
       </div>
-      <div style={{ display: "flex", flexDirection: "column", gap: "15px", width: "230px" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px",
+          width: "230px"
+        }}
+      >
         {shuffledAnswers.map((answer, index) => (
           <button
             key={index}
@@ -64,7 +79,7 @@ export const AnsweredPage = ({nextTimeCount, quizData, justQuestion, isTimeUp, i
                     : "red"
                   : "skyblue",
               opacity: 0.5,
-              boxShadow: "none",
+              boxShadow: "none"
             }}
             disabled
           >
--- src/components/TopPage.jsx
+++ src/components/TopPage.jsx
@@ -1,20 +1,34 @@
-import { useState } from 'react';
-import { globalButtonStyle, globalButtonHoverStyle } from '../styles/globalButton';
+import { useState } from "react"
+import {
+  globalButtonStyle,
+  globalButtonHoverStyle
+} from "../styles/globalButton"
 
 export const TopPage = ({ handleClick }) => {
-  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false)
 
-  const buttonStyle = {   // このコンポーネントでのボタンのスタイルを指定
+  const buttonStyle = {
+    // このコンポーネントでのボタンのスタイルを指定
     ...globalButtonStyle,
-    backgroundColor: isHovered ? globalButtonHoverStyle.backgroundColor : "violet",
-    boxShadow: isHovered ? globalButtonHoverStyle.boxShadow : globalButtonStyle.boxShadow,
-  };
+    backgroundColor: isHovered
+      ? globalButtonHoverStyle.backgroundColor
+      : "violet",
+    boxShadow: isHovered
+      ? globalButtonHoverStyle.boxShadow
+      : globalButtonStyle.boxShadow
+  }
 
   return (
     <div>
-      <h3 style={{ color: "orange", marginBottom: "20px" }}>全人類の基本をクリアせよ！</h3>
+      <h3 style={{ color: "orange", marginBottom: "20px" }}>
+        全人類の基本をクリアせよ！
+      </h3>
       <div style={{ marginBottom: "30px" }}>
-        <img src="/images/ご挨拶.png" alt="スタート画面" style={{ width: "auto", height: "250px" }} />
+        <img
+          src="/images/ご挨拶.png"
+          alt="スタート画面"
+          style={{ width: "auto", height: "250px" }}
+        />
       </div>
       <button
         onClick={handleClick}
